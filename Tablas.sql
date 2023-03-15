01405ff1fe549622b12470651756647a7eca308a
JLCaballeroMQ <jcm1017@alu.ubu.es>
2023-03-15 10:11:30 -0500

    creacion sql tablas

---
+++ Tablas.sql
@@ -0,0 +1,69 @@
+--creacion de tablas con sus respectivas llaves foraneas y sus llaves primarias
+--JLCaballeroMQ
+
+
+
+create table modelos(
+	id_modelo 		integer primary key,
+	nombre			varchar(30) not null,
+	precio_cada_dia 	numeric(6,2) not null check (precio_cada_dia>=0),
+	capacidad_deposito	integer not null check (capacidad_deposito>0),
+	tipo_combustible	varchar(10) not null references precio_combustible);
+
+
+create table vehiculos(
+	matricula	varchar(8)  primary key,
+	id_modelo	integer  not null references modelos,
+	color		varchar(10)
+);
+
+
+create table reservas(
+	idReserva	integer primary key,
+	cliente  	varchar(9) references clientes,
+	matricula	varchar(8) references vehiculos,
+	fecha_ini	date not null,
+	fecha_fin	date,
+	check (fecha_fin >= fecha_ini)
+);
+
+
+create table facturas(
+	nroFactura	integer primary key,
+	importe		numeric( 8, 2),
+	cliente		varchar(9) not null references clientes
+);
+
+create table lineas_factura(
+	nroFactura	integer references facturas,
+	concepto	char(40),
+	importe		numeric( 7, 2),
+	primary key ( nroFactura, concepto)
+);
+	
+
+create table clientes(
+	NIF	varchar(9) primary key,
+	nombre	varchar(20) not null,
+	ape1	varchar(20) not null,
+	ape2	varchar(20) not null,
+	direccion varchar(40) 
+);
+
+create table precio_combustible(
+	tipo_combustible	varchar(10) primary key,
+	precio_por_litro	numeric(4,2) not null
+);
+
+create table clientes(
+	NIF	varchar(9) primary key,
+	nombre	varchar(20) not null,
+	ape1	varchar(20) not null,
+	ape2	varchar(20) not null,
+	direccion varchar(40) 
+);
+
+create table precio_combustible(
+	tipo_combustible	varchar(10) primary key,
+	precio_por_litro	numeric(4,2) not null
+);
