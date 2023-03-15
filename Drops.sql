744df1983507355b279944c85a1d28a82569d04d
JLCaballeroMQ <jcm1017@alu.ubu.es>
2023-03-15 10:18:39 -0500

    creacion drops tablas secuencias

---
+++ Drops.sql
@@ -0,0 +1,11 @@
+drop table precio_combustible cascade constraints;
+drop table modelos            cascade constraints;
+drop table vehiculos 	      cascade constraints;
+drop table clientes 	      cascade constraints;
+drop table facturas	      cascade constraints;
+drop table lineas_factura     cascade constraints;
+drop table reservas			 cascade constraints;
+
+drop sequence seq_modelos;
+drop sequence seq_num_fact;
+drop sequence seq_reservas;
