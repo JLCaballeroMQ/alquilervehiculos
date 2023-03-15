6306c54f1b900561c9e1a51f18d297ad0e881e61
JLCaballeroMQ <jcm1017@alu.ubu.es>
2023-03-15 10:38:26 -0500

    DESARRROLLO PROCEDIEMIENTO ALQUILER

---
+++ procedimientos_Almacenados.sql
@@ -0,0 +1,65 @@
+--creacion de PROCEDIMIENTOS ALMACENADOS
+--JLCaballeroMQ
+create or replace procedure alquilar(arg_NIF_cliente varchar,
+  arg_matricula varchar, arg_fecha_ini date, arg_fecha_fin date) is
+begin
+  null;
+end;
+/
+create or replace
+procedure reset_seq( p_seq_name varchar )
+--From https://stackoverflow.com/questions/51470/how-do-i-reset-a-sequence-in-oracle
+is
+    l_val number;
+begin
+    --Averiguo cual es el siguiente valor y lo guardo en l_val
+    execute immediate
+    'select ' || p_seq_name || '.nextval from dual' INTO l_val;
+
+    --Utilizo ese valor en negativo para poner la secuencia cero, pimero cambiando el incremento de la secuencia
+    execute immediate
+    'alter sequence ' || p_seq_name || ' increment by -' || l_val || 
+                                                          ' minvalue 0';
+   --segundo pidiendo el siguiente valor
+    execute immediate
+    'select ' || p_seq_name || '.nextval from dual' INTO l_val;
+
+    --restauro el incremento de la secuencia a 1
+    execute immediate
+    'alter sequence ' || p_seq_name || ' increment by 1 minvalue 0';
+
+end;
+/
+create or replace procedure inicializa_test is
+begin
+  reset_seq( 'seq_modelos' );
+  reset_seq( 'seq_num_fact' );
+  reset_seq( 'seq_reservas' );
+        
+  
+    delete from lineas_factura;
+    delete from facturas;
+    delete from reservas;
+    delete from vehiculos;
+    delete from modelos;
+    delete from precio_combustible;
+    delete from clientes;
+   
+		
+    insert into clientes values ('12345678A', 'Pepe', 'Perez', 'Porras', 'C/Perezoso n1');
+    insert into clientes values ('11111111B', 'Beatriz', 'Barbosa', 'Bernardez', 'C/Barriocanal n1');
+    
+    insert into precio_combustible values ('Gasolina', 1.5);
+    insert into precio_combustible values ('Gasoil',   1.4);
+    
+    insert into modelos values ( seq_modelos.nextval, 'Renault Clio Gasolina', 15, 50, 'Gasolina');
+    insert into vehiculos values ( '1234-ABC', seq_modelos.currval, 'VERDE');
+
+    insert into modelos values ( seq_modelos.nextval, 'Renault Clio Gasoil', 16,   50, 'Gasoil');
+    insert into vehiculos values ( '1111-ABC', seq_modelos.currval, 'VERDE');
+    insert into vehiculos values ( '2222-ABC', seq_modelos.currval, 'GRIS');
+	
+    commit;
+end;
+/
+exec inicializa_test;
